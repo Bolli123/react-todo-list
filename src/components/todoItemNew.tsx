fa1524da23b4ad64ef62630616e18e8a3fcb7dca
Bolli123 <bjossi34@gmail.com>
2022-07-04 23:12:21 +0000

    fixed margin error

--- src/components/todoItemNew.tsx
+++ src/components/todoItemNew.tsx
@@ -23,7 +23,7 @@ function NewTodoItem(props : NewTodoProps) {
             <input autoFocus className="todoInfo text-2xl grow focus:outline-none mr-5 w-6" value={name} type="text" onKeyPress={(e) => handleKeyPress(e)} onChange={(e) => setName(e.target.value)}/>
             <button type="button" className="bg-rose-600 text-white font-medium p-1 px-3 cursor-pointer hover:scale-110 transition-all 1s" onClick={() => handleSubmit()}>Add!</button>
         </div>
-        <div className="mx-5 mb-5 italic text-base text-slate-500">{Moment(props.date).format('ddd, MMMM D YYYY')}</div>
+        <div className="ml-12 mb-5 italic text-base text-slate-500">{Moment(props.date).format('ddd, MMMM D YYYY')}</div>
         <hr className="textBreak"/>
     </div>
     )
